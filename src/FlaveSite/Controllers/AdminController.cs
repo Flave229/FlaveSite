2fab9dee39100833524b8f49983018b3574df064
Flave <david@flave.co.uk>
2017-06-30 21:48:24 +0100

    Adding an admin section of the website. So far only the navbar is done with Font Awesome added to the external libraries to display icons.

---
+++ src/FlaveSite/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlaveSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
