40d1c01eb79ea40df7913cc6954368fd2850a618
David Smith <david@flave.co.uk>
2018-01-11 14:14:05 +0000

    Updating all external libraries to newest versions and fixing a supressed startup error by giving the project and explicit reference to HostingStartup.

--- src/FlaveSite/Startup.cs
+++ src/FlaveSite/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using FlaveSite.Core.Blogs;
 using FlaveSite.Core.Projects;
 using Microsoft.AspNetCore.Builder;
